04db1b9955c1986bb3a94de42c1f01c4f787f0bb
Bruno Nunes <brunocmo.nunes@gmail.com>
2020-04-29 18:48:31 -0300

    List projects of API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,51 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { SafeAreaView, FlatList , Text, StyleSheet, StatusBar } from 'react-native';
+
+import api from './services/api';
 
 export default function App() {
-  return <View />;
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(res => {
+      console.log(res.data);
+      setProjects(res.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+
+      <SafeAreaView style={styles.container}>
+      <FlatList 
+        data={projects}
+        keyExtractor={project => project.id}
+        renderItem={({ item: project }) => (
+          <Text style={styles.project}>{project.title}</Text>
+        )}
+      />
+
+      </SafeAreaView>
+
+      {/* <View style={styles.container}>
+        {projects.map(project => (
+          <Text style={styles.project} key={project.id}>{project.title}</Text>
+        ))}
+      </View> */}
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+
+  },
+
+  project: {
+    color: '#FFF',
+    fontSize: 30,
+  },
+});

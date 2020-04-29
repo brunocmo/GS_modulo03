ffd343229189c3df01d8024bbbafa627d592df3f
Bruno Nunes <brunocmo.nunes@gmail.com>
2020-04-29 19:03:46 -0300

    Create new projects

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { SafeAreaView, FlatList , Text, StyleSheet, StatusBar } from 'react-native';
+import { SafeAreaView, FlatList , Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
 
 import api from './services/api';
 
@@ -13,19 +13,38 @@ export default function App() {
     });
   }, []);
 
+  async function handleAddProject() {
+    const res = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: 'Bruno Nunes',
+    });
+
+    const project = res.data;
+
+    setProjects([...projects, project]);
+  }
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
 
       <SafeAreaView style={styles.container}>
-      <FlatList 
-        data={projects}
-        keyExtractor={project => project.id}
-        renderItem={({ item: project }) => (
+
+        <FlatList
+          data={projects}
+          keyExtractor={project => project.id}
+          renderItem={({item: project}) => (
           <Text style={styles.project}>{project.title}</Text>
         )}
-      />
+        />
 
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={styles.button}
+          onPress={handleAddProject}
+        >
+          <Text style={styles.buttonText}>Adicionar projeto</Text>
+        </TouchableOpacity>
       </SafeAreaView>
 
       {/* <View style={styles.container}>
@@ -47,5 +66,19 @@ const styles = StyleSheet.create({
   project: {
     color: '#FFF',
     fontSize: 30,
+  },
+
+  button: {
+    backgroundColor: '#FFF',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
